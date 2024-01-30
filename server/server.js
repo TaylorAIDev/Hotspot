8eb774459aaddccdc85319b12101a5dcbfad0f9b
dragon <haojiang1029@gmail.com>
2024-01-30 23:00:05 +0900

    change port 5000

--- server/server.js
+++ server/server.js
@@ -180,7 +180,7 @@ app.get('/api/chat',verifyToken, async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
